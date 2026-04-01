e6e1728f2235d6ab3ded7a67bdaa5120fb939b4d
Shane McClain <hide579c@gmail.com>
2026-04-01 10:58:43 -0500

    simple resnet init

---
+++ NumberIdentification/simple_resnet.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import numpy as np
+
+class SimpleResNet(nn.Module):
+    def __init__(self, input_size=(28, 28), hidden_size=128, num_classes=10):
+        super().__init__()
+        _input_size = np.prod(input_size)
+
+        self.main_structure = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        return self.main_structure(x)
